4686a096723f83944776350114347f744299ac85
Michael Roose <mjroose@gmail.com>
2018-12-16 18:12:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function iterativeLog(array) {
     console.log(`${index}: ${element}`);
   })
 }
+
+function iterate(callback) {
+  array = ["Homer", "Marge", "Bart", "Lisa", "Maggie";
+  array.forEach(callback);
+  return array;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
